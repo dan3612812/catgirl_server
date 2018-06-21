25b0b9d6dc1eee0defbb86c7ac30037752af198b
dan3612812 <dan3612812@gmail.com>
2018-06-22 02:21:02 +0800

    fix clog and await conf.chrifo  and judgment mstatus==undefind && roomcou==-1 exit

--- src/clog.js
+++ src/clog.js
@@ -1,7 +1,11 @@
 //flags 來源檔案名稱 或者自定義
 //msg 預印出訊息
 //tag 其他標記或分類
-
+/* 
+    mqtt 0:server msg
+    mqtt 3:json parse error
+    mqtt 4:status undefind
+*/
 exports.log = function (flags, msg, tag) {
     if (flags == "controller") {
         console.log("*********");
@@ -12,6 +16,14 @@ exports.log = function (flags, msg, tag) {
             console.log("//////////");
             console.log(msg);
             console.log("//////////");
+        } else if (tag == "3") {
+            console.log("┏----------------┓");
+            console.log("┃JSON parse error┃");
+            console.log("┗----------------┛");
+        } else if (tag == "4") {
+            console.log("┏----------------┓");
+            console.log("┃STATUS  undefind┃");
+            console.log("┗----------------┛");
         } else {
             if (tag == "0") { //server msg mqtt
                 console.log("-***-***-***-SERVER MSG-***-***-***-");
@@ -19,7 +31,7 @@ exports.log = function (flags, msg, tag) {
                 console.log("--clinet msg--");
             }
             console.log(msg);
-            console.log("--------------------");
+            console.log("-------------------------");
         }
     }
 }
--- src/mqtt.js
+++ src/mqtt.js
@@ -1,49 +1,61 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://47.74.20.158:1883');
+var client = mqtt.connect('mqtt://127.0.0.1:1883');
 var cont = require('./controller');
 var clog = require('./clog');
 var tfn = "mqtt";
 var rinfo = cont.rinfo; //讀取 controller.rinfo 變數
 
-
 client.on('connect', function () {
     client.subscribe('catgirl_room');
     client.subscribe('catgirl/room/p1');
 });
-
 // ////////test //////////
 // var temp = { "id": "p1", "status": "6", "test1": "t1", "test2": "t2" };
 // client.publish('catgirl_room', JSON.stringify(temp), { qos: 1 });
 ////////////////////
 
-client.on('message', function (topic, message) {
+client.on('message', async function (topic, message) {
 
     //---- 監聽 mqtt room ----//
-    var msg = JSON.parse(message);
+    try {
+        var msg = JSON.parse(message);
+    } catch (error) {
+        clog.log(tfn, "parse err", 3);
+        return //離開
+    }
     var mstatus = msg.status; //0:待機 1:準備中 2:開始 3:斷線 4:戰鬥中 5:結束 
     var mid = msg.id;
     var mtopic = topic.split("/").pop();
     var roomcou = rinfo.findIndex(x => x.roomname === mtopic);// find room index
-
     var tmsg = "";
-    if (msg.command == undefined) { //clinet msg
-        tmsg = "room_serial:" + roomcou;
-        tmsg += "\ntopic:" + mtopic + "\nid:" + mid + "\nstatus:" + mstatus;
-        tmsg += "\n" + JSON.stringify(msg);
-        clog.log(tfn, tmsg, 1);
+
+    //Judgment START------------------------
+    //判斷錯誤格式或未完整JSON包或參數錯誤 直接離開
+    if (msg.command == undefined) {
+        if (mstatus == undefined) { //錯誤格式
+            clog.log(tfn, "4", 4);
+        } else {                      //clinet msg
+            console.log("roomcou:" + roomcou);
+            tmsg = "room_serial:" + (roomcou == -1 ? "ERROR" : roomcou);
+            tmsg += "\ntopic:" + mtopic + "\nid:" + mid + "\nstatus:" + mstatus;
+            //tmsg += "\n" + JSON.stringify(msg);
+            clog.log(tfn, tmsg, 1);
+        }
     } else {                        //server msg        
         tmsg = "\nid:" + mid + "command:" + msg.command; + "\ntopic:" + mtopic;
         clog.log(tfn, tmsg, 0);
     }
+    if (roomcou == -1) { return }
+    //Judgment END---------------------------
 
     //收到兩個start 的 topic 才發送 startgame
     //收到client的訊息 就幫他傳送一次訊息
     if (mstatus == 1) {
         if (rinfo[roomcou].player1 == mid) {
-            cont.chrinfo(roomcou, "p1status", "1");
+            await cont.chrinfo(roomcou, "p1status", "1");
         };
         if (rinfo[roomcou].player2 == mid) {
-            cont.chrinfo(roomcou, "p2status", "1");
+            await cont.chrinfo(roomcou, "p2status", "1");
         };
         //雙方都準備好 由server 發訊息 開始
         if (rinfo[roomcou].p1status == "1" && rinfo[roomcou].p2status == "1") {
@@ -64,17 +76,15 @@ client.on('message', function (topic, message) {
 
     //房主離開 刪除房間
     if (mstatus == 3) {
-        if (roomcou > -1) {
-            if (rinfo[roomcou].roomname == mtopic && mtopic == mid) {
-                clog.log(tfn, "del room roomname:" + rinfo[roomcou].roomname + "\n this room is exist", 2);
-                rinfo.splice(roomcou, 1);
-                // 通知 clinet
-                var temp = {
-                    "id": "server",
-                    "command": "leave"
-                }
-                client.publish(topic, JSON.stringify(temp), { qos: 2 });
+        if (rinfo[roomcou].roomname == mtopic && mtopic == mid) {
+            clog.log(tfn, "del room roomname:" + rinfo[roomcou].roomname + "\n this room is exist", 2);
+            rinfo.splice(roomcou, 1);
+            // 通知 clinet
+            var temp = {
+                "id": "server",
+                "command": "leave"
             }
+            client.publish(topic, JSON.stringify(temp), { qos: 2 });
         }
     }
 
@@ -86,9 +96,6 @@ client.on('message', function (topic, message) {
             "command": "end"
         }
         client.publish(topic, JSON.stringify(temp), { qos: 2 });
-        //刪除陣列資料
-        //   if (roomcou > -1) { clog.log(tfn, "this room game over del room", 2); rinfo.splice(roomcou, 1) };
-
     }
 
 
@@ -116,19 +123,20 @@ client.on('message', function (topic, message) {
     }
     if (mstatus == "33") {
         var temp = { "": "" };
-        cont.chrinfo(0, temp);
+        await cont.chrinfo(0, temp);
         console.log(rinfo[0]);// 會等上面做完 才console.log
     }
     if (mstatus == "22") {
-        cont.chrinfo(0, "player1", "123");
+        await cont.chrinfo(0, "player1", "123");
         console.log("this 22");
     }
     //check  var
     if (mstatus == "11") {
         console.log(" check p1status:" + rinfo[0].p1status);
     }
-    //----------------this client.on to test end----------------//
+    //----------------this client.on to test END----------------//
 });
+
 //告知該topic 有人加入房間
 exports.jroom = function (topic, twoid) {
     var temp = {
@@ -157,7 +165,7 @@ exports.addsubcribe = function (room) {
 //----------not use-----------//
 //推訊息
 exports.mqttpub = function (topic, message) {
-    client.publish('catgirl/' + topic, message, { qos: 1 });
+    client.publish('catgirl/' + topic, message, { qos: 2 });
 }
 
 exports.createroom = function (roomid) {
@@ -166,7 +174,7 @@ exports.createroom = function (roomid) {
         instr: 'cr',
         roomid: roomid
     }
-    client.publish('catgirl_roomlist', JSON.stringify(temp), { qos: 1 });
+    client.publish('catgirl_roomlist', JSON.stringify(temp), { qos: 2 });
 }
 
 exports.delroom = function (roomid, roomname) {
@@ -175,5 +183,5 @@ exports.delroom = function (roomid, roomname) {
         roomid: roomid,
         roomname: roomname
     }
-    client.publish('catgirl_roomlist', JSON.stringify(temp), { qos: 1 });
+    client.publish('catgirl_roomlist', JSON.stringify(temp), { qos: 2 });
 }
